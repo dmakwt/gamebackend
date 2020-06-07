529070f60727f1190493be01552db01f073c1ac2
Nasser Alenezi <nass_181@hotmail.com>
2020-06-07 22:50:46 +0300

    Added User model

---
+++ src/models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw Error('Email is invalid')
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+    },
+
+    token: {
+        type: String,
+        required: true
+    },
+
+},
+    {
+        timestamps: true,
+    }
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+const User = mongoose.model('User', userSchema)
+
+
+
+module.exports = User
+
+
+
+
+
+
