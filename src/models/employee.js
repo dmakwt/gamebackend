42584c3f2974277244cefb4346222d2e21bc83c4
Dhari <dmakwt@gmail.com>
2020-06-07 16:27:16 -0500

    added employee model

---
+++ src/models/employee.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+
+
+const employeeSchema = new mongoose.Schema({
+
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: 'User'
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw Error('Email is invalid')
+            }
+        }
+    },
+    position: {
+        type: String,
+        required: true
+    }
+})
