da5911123ab2180fba5d3a7a499d4c5278dca06e
Nasser Alenezi <nass_181@hotmail.com>
2020-06-12 10:59:45 +0300

    Fixed HTML styles, added JS code that handles game info, fixed the reset.js code with a trycatch method, added CSS file

---
+++ public/js/game_info.js
@@ -0,0 +1,14 @@
+const gamenamePlace = document.querySelector('#gamename')
+
+
+
+
+const Year = '2020'
+const Game_Name = 'THE BEST GAME'
+
+
+
+
+
+
+gamename.innerHTML= `©${Year} ${Game_Name}`
--- public/js/reset.js
+++ public/js/reset.js
@@ -1,15 +1,24 @@
 const resetPasswordAPI = async()=>{
-	const link = window.location.href
-	const userNewPassword = document.querySelector('#password1').value
-	const resetPasswordFetch = await fetch(link, {
-    method: 'POST',
-    headers: {
-      'newpassword': userNewPassword,
-    }
-  })
-  
-  
-  return resetPasswordFetch.status
+
+	try {
+		const link = window.location.href
+		const userNewPassword = document.querySelector('#password1').value
+		const resetPasswordFetch = await fetch(link, {
+		method: 'POST',
+		headers: {
+		  'newpassword': userNewPassword,
+		}
+	  })
+	  
+	
+	  
+	  return resetPasswordFetch.status
+	} catch (error) {
+		location.reload();
+	}
+
+
+
 }
 
 
@@ -21,7 +30,6 @@ form.addEventListener('submit', async(e) => {
 	e.preventDefault()
 	const responseStatus = await resetPasswordAPI()
 
-	
 	//handle succesful fetch [[     URL NEED TO BE CHANGE   ]]
 	if(responseStatus === 200){
 		const link = "http://localhost:3000/users/successful"
